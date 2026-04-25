131c413c415ea3fe6c442562c253fbe1046cee31
Dalton Knapp <knapptime_wv@outlook.com>
2026-04-25 17:08:14 -0400

    Update consumer.lua

--- consumer.lua
+++ consumer.lua
@@ -1,10 +1,28 @@
 DNS_PORT = 53
 HOSTNAME_PATH = "/etc/hostname"
+RESOLV_PATH = "/etc/resolv"
 
 local f = assert(io.open(HOSTNAME_PATH))
 hostname = f:read()
 f:close()
 
-for modem in peripherals.find("modem") do
-  if modem.isOpen(53) then
+local f = assert(io.open(RESOLV_PATH))
+servers = {}
+for line in f:lines() do
+    table.insert(t=servers, value=line)
 end
+f:close()
+
+-- Nake sure DNS nameservers are found
+for s in servers do
+    if rednet.lookup("DNS", s) then
+        found = 1
+    end
+end
+-- Maybe do some work here with nameservers
+
+-- Host the DNS server on this
+for modem in { peripherals.find("modem") } do
+    modem.open(53)
+end
+rednet.host("DNS", hostname)
