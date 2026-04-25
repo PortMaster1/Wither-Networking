1f0a74bd808bdb38c69c1132077b404b33d85f98
Dalton Knapp <knapptime_wv@outlook.com>
2026-04-25 16:36:51 -0400

    Update consumer.lua

--- consumer.lua
+++ consumer.lua
@@ -1,1 +1,10 @@
 DNS_PORT = 53
+HOSTNAME_PATH = "/etc/hostname"
+
+local f = assert(io.open(HOSTNAME_PATH))
+hostname = f:read()
+f:close()
+
+for modem in peripherals.find("modem") do
+  if modem.
+end
