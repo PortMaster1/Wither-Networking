b681a621f73b4f6bbe4e2d80dc1eb32f279658a9
Dalton Knapp <knapptime_wv@outlook.com>
2026-04-27 13:54:21 -0400

    Update packyinstaller.lua

--- packyinstaller.lua
+++ packyinstaller.lua
@@ -1,5 +1,13 @@
-if not fs.exists("./pacman.lua") then
-    return "File ./pacman.lua not found, please place files in same directory and try again. Exiting with exit code 1"
+print("Installing packy...")
+
+-- Checks if packy module exists
+if not fs.exists("./packy.lua") then
+    return "File ./packy.lua not found, please place files in same directory and try again. Exiting with exit code 1"
+end
+
+-- Creates the directory for installed packages
+if not fs.exists("/usr/bin") then
+    fs.makeDir("/usr/bin")
 end
 
 -- Script adds /usr/bin to path on startup
@@ -9,8 +17,14 @@ if not fs.exists("/rom/autorun/packypath.lua") then
     f.close()
 end
 
+-- Moves packy.lua into packages directory
+if not fe.exists("/usr/bin/packy.lua") then
+    fs.move("./packy.lua", "/usr/bin/packy.lua")
+end
 
 -- Finally
 if not string.find(shell.path(), "/usr/bin" then
     shell.setPath(shell.path() .. ":/usr/bin")
 end
+
+print("Successfully installed packy, you can now delete this file.")
