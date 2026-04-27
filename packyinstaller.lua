0b15eb36324b680c97dcdec15f17e3a93d68978e
Dalton Knapp <knapptime_wv@outlook.com>
2026-04-27 13:46:09 -0400

    Update packyinstaller.lua

--- packyinstaller.lua
+++ packyinstaller.lua
@@ -1,3 +1,8 @@
+if not fs.exists("./pacman.lua") then
+    return "File ./pacman.lua not found, please place files in same directory and try again. Exiting with exit code 1"
+end
+
+-- Script adds /usr/bin to path on startup
 if not fs.exists("/rom/autorun/packypath.lua") then
     local f = fs.open("/rom/autorun/packypath.lua", "w")
     f.write(shell.setPath(shell.path() .. ":/usr/bin")
