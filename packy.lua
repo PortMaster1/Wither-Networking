03136ed569099b39094e4fe61d4f8ac5fbf74eba
Dalton Knapp <knapptime_wv@outlook.com>
2026-04-27 14:44:39 -0400

    Update packy.lua

--- packy.lua
+++ packy.lua
@@ -3,3 +3,12 @@ if #tArgs < 1 then
     return
 end
 
+local function install(pkg_name)
+    print("Checking package repo...")
+    local response = http.get(("https://github.com/PortMaster1/Wither-Networking/contents/%d"):format(pkg_name))
+    if response == 404 then
+        print("Package not found.")
+        return
+    end
+    local file = http.get(("https://raw.githubusercontent.com/PortMaster1/Wither-Networking/main/%d"):format(pkg_name))
+end
