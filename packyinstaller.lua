30376c84c3b039cfd3a56b1284c511a1c45e041f
Dalton Knapp <knapptime_wv@outlook.com>
2026-05-02 16:33:17 -0400

    Update packyinstaller.lua

--- packyinstaller.lua
+++ packyinstaller.lua
@@ -1,38 +1,8 @@
 print("Initializing Installer")
 
-local MIRRORS = {
-    "https://api.github.com/repos/PortMaster1/Wither-Networking/contents/",
-    "https://api.github.com/repos/park102/cc/contents/"
-}
+local MIRROR = "https://api.github.com/repos/PortMaster1/Wither-Networking/contents/"
 
--- Creates the directory for mirrorlist
-if not fs.exists("/etc/packy") then
-    fs.makeDir("/etc/packy")
-end
-print("/etc/packy creation confirmed")
 
--- Writes mirrorlist
-if not fs.exists("/etc/packy/mirrorlist.txt") then
-    local f = fs.open("/etc/packy/mirrorlist.txt","w+")
-    for _, url in ipairs(MIRRORS) do
-        print(url)
-        f.writeLine(url)
-    end
-    f.close()
-end
-print("Mirrorlist added")
-
--- Creates the directpry for temporary files
-if not fs.exists("/tmp") then
-    fs.makeDir("/tmp")
-end
-print("/tmp creation confirmed")
-
--- Creates the directory for installed packages
-if not fs.exists("/usr/bin/packy") then
-    fs.makeDir("/usr/bin/packy")
-end
-print("/usr/bin creation confirmed")
 
 -- Script adds /usr/bin to path on startup
 if not fs.exists("/startup/packypath.lua") then
@@ -52,9 +22,9 @@ end
 
 -- Checks if packy module exists
 if not fs.exists("/usr/bin/packy.lua") then
-    local build = request(MIRRORS[1] .. "packy/PKGBUILD.json")
+    local build = request(MIRROR .. "packy/PKGBUILD.json")
     for rpath, spath in pairs(build["paths"]) do
-        local response = request(MIRRORS[1] .. rpath)
+        local response = request(MIRRORS .. rpath)
         local file = http.get(response["download_url"])
         fs.makeDir(spath) -- Will make a directory where the file goes
         fs.delete(spath) -- Deletes the directory claiming to be the file
